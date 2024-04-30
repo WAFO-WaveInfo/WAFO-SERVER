b36fb09b10336b8803e87607bd8c5904bfd6d5d0
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-04-30 15:27:12 +0900

    feat: folder InitDB 추가
    - Folder에 InitDB를 위한 temp constructor 추가
    - FolderCreateResponse 필드 명칭 수정
      - username → nickname

--- wafo-server/src/main/java/wafo/wafoserver/InitDB.java
+++ wafo-server/src/main/java/wafo/wafoserver/InitDB.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import wafo.wafoserver.account.user.User;
 import wafo.wafoserver.account.user.repository.UserRepository;
+import wafo.wafoserver.folder.Folder;
+import wafo.wafoserver.folder.repository.FolderRepository;
 
 @Component
 @RequiredArgsConstructor
@@ -25,11 +27,14 @@ public class InitDB {
     static class InitService {
 
         private final UserRepository userRepository;
+        private final FolderRepository folderRepository;
 
         public void dbInit1() {
             Long userId1 = userRepository.save(new User("test1@wafo.io", "user1", "testUser1"));
             Long userId2 = userRepository.save(new User("test2@wafo.io", "user2", "testUser2"));
             Long userId3 = userRepository.save(new User("test3@wafo.io", "user3", "testUser3"));
+
+            Long folderId = folderRepository.save(new Folder("defaultFolder", userRepository.findById(userId1).get()));
         }
     }
 }
