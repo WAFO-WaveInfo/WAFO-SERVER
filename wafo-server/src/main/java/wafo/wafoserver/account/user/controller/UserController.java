3c181837dda713dc42b229c793fe2c994cce3c44
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-05-26 22:56:35 +0900

    feat: ngrok 적용
    - Controller 에 ngrok 설정
    - CORS 설정
    
    resolve: #8

--- wafo-server/src/main/java/wafo/wafoserver/account/user/controller/UserController.java
+++ wafo-server/src/main/java/wafo/wafoserver/account/user/controller/UserController.java
@@ -2,6 +2,7 @@ package wafo.wafoserver.account.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wafo.wafoserver.account.user.repository.UserRepository;
@@ -10,6 +11,7 @@ import wafo.wafoserver.account.user.repository.UserRepository;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
     private final UserRepository userRepository;
 }
